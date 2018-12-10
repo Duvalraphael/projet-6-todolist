8ee1209a57530753b41585bac1ae99d5ec71df90
Raphaël <duvalraphael1995@gmail.com>
2018-12-10 16:54:23 +0100

    debut materialize

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <!--Import Google Icon Font-->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="assets/materialize/css/materialize.min.css"  media="screen,projection"/>
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>To Do List</title>
@@ -15,24 +15,21 @@
     <body>
         <div class="container">
             <div class="row">
-                <div class="col s12">
+                <form class="col s12">
                     <div class="row">
-                        <div class="input-field col s12">
-                        <p>
-      <label>
-        <input type="checkbox" class="filled-in" checked="checked" />
-        <span>Filled in</span>
-      </label>
-                            <input type="text" id="autocomplete-input" class="autocomplete">
-                            <label for="autocomplete-input">Autocomplete</label>
+                        <h3>Ajouter une tâche </h3>
+                        <div class="input-field col s6">
+                            <input id="tache_input" type="text" class="validate">
+                            <label for="tache_input">la tâche à effectuer : </label>
                         </div>
                     </div>
-                </div>
+                    <button class="btn waves-effect waves-light" type="submit" name="Ajouter">Ajouter
+                        <i class="material-icons right">add_circle</i>
+                    </button>
+                </form>
             </div>
         </div>
-
-
         <!--JavaScript at end of body for optimized loading-->
-        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <script type="text/javascript" src="assets/materialize/js/materialize.min.js"></script>
     </body>
 </html>
